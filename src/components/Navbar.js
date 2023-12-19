f12a992662c383fe2a2082734bbdabae8555f8a2
najm-choueiry <nachoueiry@outlook.com>
2023-12-19 16:39:48 +0200

    First glance at the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt="Logo" className="logo-image-navbar" />
+      </Link>
+    </Stack>
+  );
 }
 
 export default Navbar;
