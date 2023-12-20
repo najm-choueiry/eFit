e1ccefe8a69d2b747c243c4d3d1379f0d70b2020
najm-choueiry <nachoueiry@outlook.com>
2023-12-20 03:28:20 +0200

    Implementing the search field and search button

--- src/components/SearchExercices.js
+++ src/components/SearchExercices.js
@@ -14,18 +14,32 @@ function SearchExercices() {
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
-          height="76px"
           value=""
           onChange={(e) => {}}
           placeholder="Search Exercises"
           type="text"
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "1170px", xs: "350px" },
+            width: { lg: "800px", xs: "350px" },
             backgroundColor: "#fff",
             borderRadius: "40px",
           }}
         />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            ml: "2%",
+          }}
+        >
+          Search
+        </Button>
       </Box>
     </Stack>
   );
