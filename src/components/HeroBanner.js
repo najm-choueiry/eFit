19905a0623a7dca0cb7362c78aa82eac3e00f2e4
najm-choueiry <nachoueiry@outlook.com>
2023-12-20 03:01:48 +0200

    Finalizing the HeroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -22,15 +22,36 @@ function HeroBanner() {
             xs: "40px",
           },
         }}
+        mb="23px"
+        mt="30px"
       >
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+      <Typography
+        fontSize="22px"
+        lineHeight="35px"
+        mb={4}
+        sx={{
+          backgroundColor: "#ff2625",
+          padding: "10px",
+        }}
+      >
         Checkout the most effective exercises
       </Typography>
       <Button variant="contained" color="error" href="#exercises">
         Explore Exercises
       </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+        fontSize="200px"
+      >
+        Exercise
+      </Typography>
       <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
