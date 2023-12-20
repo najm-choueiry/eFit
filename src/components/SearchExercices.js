8e0a6e1ab164781ca81f9debf27dbf72419b577b
najm-choueiry <nachoueiry@outlook.com>
2023-12-20 03:07:18 +0200

    Coding the title for the SearchExercises component

--- src/components/SearchExercices.js
+++ src/components/SearchExercices.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 function SearchExercices() {
-  return <div>SearchExercices</div>;
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awsome Exercises You <br /> Should Know
+      </Typography>
+    </Stack>
+  );
 }
 
 export default SearchExercices;
