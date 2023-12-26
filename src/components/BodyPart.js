af039edd0f6b7d41af46b321749dd833989c9f67
najm-choueiry <nachoueiry@outlook.com>
2023-12-26 17:51:58 +0200

    <> Finalizing the bodyparts components and making them horizontal scroll

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -20,12 +20,20 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
     >
-      <img
-        src={Icon}
-        alt="dumbell"
-        styles={{ width: "140px", height: "140px" }}
-      />
+      <img src={Icon} alt="dumbell" style={{ width: "40px", height: "40px" }} />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
