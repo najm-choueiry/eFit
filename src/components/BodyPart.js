d5139f89d4bae738709f5e2a8590222acbbcb3af
najm-choueiry <nachoueiry@outlook.com>
2023-12-26 17:42:42 +0200

    <> Finalizing the bodyparts components and making them horizontal scroll

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,14 +2,24 @@ import React from "react";
 import { Stack, Typography } from "@mui/material";
 
 import Icon from "../assets/icons/gym.png";
+import { BorderTop } from "@mui/icons-material";
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
       type="button"
       alignItems="center"
       justifyContent="center"
       className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
     >
       <img
         src={Icon}
--- src/components/SearchExercices.js
+++ src/components/SearchExercices.js
@@ -11,7 +11,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        // "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
         excerciseOptions
       );
 
@@ -24,7 +24,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const handleSearch = async () => {
     if (search) {
       const exercisesData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises",
+        // "https://exercisedb.p.rapidapi.com/exercises",
         excerciseOptions
       );
 
