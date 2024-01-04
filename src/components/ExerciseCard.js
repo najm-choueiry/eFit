d60df13c41f91e29b9af993a8363f48c7d8b659d
najm-choueiry <nachoueiry@outlook.com>
2024-01-04 19:00:34 +0200

    <> Working on the ExerciseCard

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Typography, Stack } from "@mui/material";
 
-function ExerciseCard() {
-  return <div>ExerciseCard</div>;
+function ExerciseCard({ exercise }) {
+  return (
+    <Link to={`/exercise/${exercise.id}`} className="exercise-card">
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    </Link>
+  );
 }
 
 export default ExerciseCard;
