4aee68b0a3a330e742a86c8e6ce80b9ea26f7dbf
najm-choueiry <nachoueiry@outlook.com>
2024-01-04 16:09:52 +0200

    <> Completing the exercises sections

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,12 +4,24 @@ import { Box, Stack, Typography } from "@mui/material";
 
 import { excerciseOptions, fetchData } from "../utils/fetchData";
 
+import ExerciseCard from "./ExerciseCard.JS";
+
 function Exercises({ exercises, bodyPart, setExercises }) {
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
         Showing Results
       </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises?.map((exercise, index) => {
+          <ExerciseCard key={index} exercise={exercise} />;
+        })}
+      </Stack>
     </Box>
   );
 }
