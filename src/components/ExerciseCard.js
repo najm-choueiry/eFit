474610e0149e204a8c3fff4ba98ead004db5ddd3
najm-choueiry <nachoueiry@outlook.com>
2024-01-23 19:46:53 +0200

    <> Adding the body part and target muscle

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -6,6 +6,33 @@ const ExerciseCard = ({ exercise }) => {
   return (
     <Link to={`/exercise/${exercise.id}`} className="exercise-card">
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fcc757",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
     </Link>
   );
 };
