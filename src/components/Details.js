11f2b7d9d06106debff8086067bb0d60a889a32f
najm-choueiry <nachoueiry@outlook.com>
2024-01-25 16:10:26 +0200

    <> Adding the extra details for each exercise

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,6 +8,21 @@ import EquipmentImage from "../assets/icons/equipment.png";
 const Details = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
+
   return (
     <Stack
       gap="60px"
@@ -15,12 +30,27 @@ const Details = ({ exerciseDetail }) => {
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
-        <Typography>{name}</Typography>
-        <Typography>
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h6">
           Exercises keep you strong.
           {name} bend is one of the best exercises to target your {target}. It
           will help you improve your mood and gain energy.
         </Typography>
+        {extraDetail.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            <Button
+              sx={{
+                background: "#fff2db",
+                borderRadius: "50%",
+                width: "100px",
+                height: "100px",
+              }}
+            >
+              <img src={item.icon} alt={bodyPart} />
+            </Button>
+            <Typography variant="h5">{item.name}</Typography>
+          </Stack>
+        ))}
       </Stack>
     </Stack>
   );
