00dbc77c6d00b580754a5f9fdf6212a3d6061251
najm-choueiry <nachoueiry@outlook.com>
2024-01-25 14:30:07 +0200

    <> Defining some variables for the exerciseDetails component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,6 +8,11 @@ import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {}, [id]);
+
   return (
     <Box>
       <Details />
