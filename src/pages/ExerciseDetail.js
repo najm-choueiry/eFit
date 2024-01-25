4e132f103cecfe8191a7113208f28844092da50f
najm-choueiry <nachoueiry@outlook.com>
2024-01-25 14:22:06 +0200

    <> Starting with the exercise details component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { fetchData, exerciseOptions } from "../utils/fetchData";
 
 function ExerciseDetail() {
   return <div>ExerciseDetail</div>;
