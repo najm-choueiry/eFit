2eda1bfa619068983720f3581b42e51cf547ace2
najm-choueiry <nachoueiry@outlook.com>
2024-01-28 11:42:27 +0200

    <> Adding a title for each path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import ExerciseDetail from "./pages/ExerciseDetail";
@@ -9,6 +9,18 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = "eFit Application - Home";
+    } else if (location.pathname.startsWith("/exercise/")) {
+      document.title = "eFit Application - Exercise Details";
+    } else {
+      document.title = "eFit Application";
+    }
+  }, [location.pathname]);
+
   return (
     <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
