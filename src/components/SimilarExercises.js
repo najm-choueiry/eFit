68bf0246f50688e8f8113af6a54f9c003e13095f
najm-choueiry <nachoueiry@outlook.com>
2024-01-28 15:01:41 +0200

    <> Adding the similar equipment

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -16,6 +16,17 @@ const SimilarExercises = ({ equipmentExercises, targetMusleExercises }) => {
           <Loader />
         )}
       </Stack>
+
+      <Typography variant="h3" mb={5} mt={7}>
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction={"row"} sx={{ p: 2, position: "relative" }}>
+        {equipmentExercises.length ? (
+          <HorizontalScrollbar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
     </Box>
   );
 };
